a9be0eeb378fa8f122fcbe75f31cd8f1a70a3262
taaha3244 <taaha.com@gmail.com>
2025-03-10 01:46:27 +0500

    Routers in Fast API

---
+++ src/books/schemas.py
@@ -0,0 +1,37 @@
+import uuid
+from datetime import date, datetime
+from typing import List
+
+from pydantic import BaseModel
+
+
+
+
+class Book(BaseModel):
+    uid: uuid.UUID
+    title: str
+    author: str
+    publisher: str
+    published_date: date
+    page_count: int
+    language: str
+    created_at: datetime
+    update_at: datetime
+
+
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
